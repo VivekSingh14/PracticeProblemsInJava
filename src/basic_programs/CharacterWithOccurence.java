3522dcea391a911d480d7215277d20c967802273
vicky1411 <vivek.singh141@yahoo.com>
2021-03-12 22:38:50 +0530

    added  print characters with occurences with optimization from o(n)2 to o(n)

---
+++ src/basic_programs/CharacterWithOccurence.java
@@ -0,0 +1,34 @@
+package basic_programs;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterWithOccurence {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str = sc.nextLine();
+        
+        char arr[] = str.toCharArray();
+
+        Map<Character, Integer> map = new HashMap();
+
+        for(int i=0; i<arr.length;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i], 1);
+            }else{
+                int val = map.get(arr[i]);
+                val++;
+                map.put(arr[i], val);
+            }
+        }
+
+        for (char c : map.keySet()) {
+           System.out.print(c +""+map.get(c)); 
+        }
+        System.out.println();
+
+        
+    }
+}
