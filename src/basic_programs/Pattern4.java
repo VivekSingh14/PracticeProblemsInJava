9fd1555ad5f747e1dbbe93555b570f9aa3227a35
vicky1411 <vivek.singh141@yahoo.com>
2021-03-14 00:28:11 +0530

    added pattern4 with version 0.0.1.4

---
+++ src/basic_programs/Pattern4.java
@@ -0,0 +1,22 @@
+package basic_programs;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int rows = sc.nextInt(); 
+        
+        for(int i =0; i<rows; i++){
+            for(int j=0;j<=i;j++ ){
+                System.out.print(" ");
+            }
+            for(int k=1; k<= rows-i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
