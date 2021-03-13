10da5e9f62f978f98bb507955b1ac7bec44c4c35
vicky1411 <vivek.singh141@yahoo.com>
2021-03-13 23:23:54 +0530

    added pattern3 with version 0.0.1.3

---
+++ src/basic_programs/Pattern3.java
@@ -0,0 +1,20 @@
+package basic_programs;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int rows = sc.nextInt();    
+        
+        for(int i=0; i<rows; i++){
+          for(int j=0;j<rows-i;j++){
+              System.out.print("* ");
+          }
+          System.out.println();
+        }
+
+    }
+    
+}
