7741dd71b56fd6cac878f9e6e92a4696a6430503
vicky1411 <vivek.singh141@yahoo.com>
2021-03-16 13:48:55 +0530

    added insertion sort with version 0.0.2.2

---
+++ src/basic_programs/InsertionSort.java
@@ -0,0 +1,23 @@
+package basic_programs;
+
+public class InsertionSort {
+    
+    public static void main(String args[]){
+        int arr[] = {0, -32, -6, -19, -33, 2, 6,1};
+
+        for (int i =1 ; i < arr.length; i++){
+                int key = arr[i];
+                int j= i-1;
+                while(j >= 0 && arr[j] > key){
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+                arr[j+1] = key;    
+        }
+
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+}
