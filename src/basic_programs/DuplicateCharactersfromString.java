08adca3fcac96592f5b547ba871d8a65600aeaf1
vicky1411 <vivek.singh141@yahoo.com>
2021-03-17 21:36:26 +0530

    added the method to print duplicates in stringwith version 0.0.2.3

---
+++ src/basic_programs/DuplicateCharactersfromString.java
@@ -0,0 +1,31 @@
+package basic_programs;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class DuplicateCharactersfromString {
+    public static void main(String args[]){
+        //Scanner sc = new Scanner(System.in);
+        //String str = sc.nextLine();
+        String str = "aabcadceklmeb";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < str.length();i++){
+            if(!map.containsKey(str.charAt(i))){
+                map.put(str.charAt(i), 1);
+            }else{
+                int value = map.get(str.charAt(i));
+                value++;
+                map.put(str.charAt(i), value);
+            }
+        }
+
+        for( char c : map.keySet()){
+            if(map.get(c) == 2){
+                System.out.println(c);
+            }
+        }
+
+    }
+}
