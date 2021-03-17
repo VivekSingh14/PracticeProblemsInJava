27c6f901de9e365a55157afa7767e1adb9140f63
vicky1411 <vivek.singh141@yahoo.com>
2021-03-17 22:23:38 +0530

    added operation to check two strings are anagram or not with version 0.0.2.4

---
+++ src/basic_programs/AnagramStrings.java
@@ -0,0 +1,31 @@
+package basic_programs;
+
+import java.util.Arrays;
+
+public class AnagramStrings {
+ public static void main(String args[]){
+     String str1 = "listen";
+     String str2 = "silent";
+
+     char strchar1[] = str1.toCharArray();
+     char strchar2[] = str2.toCharArray();
+     Arrays.sort(strchar1);
+     Arrays.sort(strchar2);
+     boolean flag  = false;
+     if(strchar1.length != strchar2.length){
+         System.out.println("Not an anagram");
+        }else {
+            for( int i = 0; i < strchar1.length;i++){
+                if(strchar1[i] != strchar2[i]){
+                flag = true;
+                break;
+                }
+            }
+        }
+        if(flag == true){
+            System.out.println("Strings are not anagram");
+        }else{
+            System.out.println("Strings are anagram");
+        }
+    }   
+}
