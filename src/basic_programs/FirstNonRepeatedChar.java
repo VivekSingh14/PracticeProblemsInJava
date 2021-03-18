c3a338b34d91548ad123a71a0261c5fdc08c9b5d
vicky1411 <vivek.singh141@yahoo.com>
2021-03-18 17:24:45 +0530

    added the operation to print the first nonrepeated character from string with version 0.0.2.6

--- src/basic_programs/FirstNonRepeatedChar.java
+++ src/basic_programs/FirstNonRepeatedChar.java
@@ -1,23 +1,22 @@
 package basic_programs;
 
-import java.util.HashMap;
-
 public class FirstNonRepeatedChar {
     public static void main(String args[]){
         String str = "GeeksforGeeks";
-        HashMap<Character, Integer> map = new HashMap<>();
-        for( int i=0; i< str.length(); i++){
-            if(!map.containsKey(str.charAt(i))){
-                map.put(str.charAt(i), 1);
-            }else{
-                int value = map.get(str.charAt(i));
-                value++;
-                map.put(str.charAt(i), value);
+        final int NO_OF_CHARS = 256;
+        char count[] = new char[NO_OF_CHARS];
+        for(int i = 0; i<str.length();i++){
+            count[str.charAt(i)]++;
+        } 
+
+        int index = -1; 
+        for(int j = 0; j <str.length(); j++){
+            if(count[str.charAt(j)]==1){
+                index = j;
+                break;
             }
         }
-        for(char c: map.keySet()){
-            System.out.println(c+ " "+ map.get(c));
-        }
+        System.out.println("first non repeated character is: "+str.charAt(index));
 
     }
 }
