5592f884c18cb8a59232176130a42ad8bfccf05f
vicky1411 <vivek.singh141@yahoo.com>
2021-04-06 23:17:59 +0530

    adding the prototype design pattern, ColorStore added with version 0.0.3.16

---
+++ src/DesignPatterns/Creational/prototype/ColorStore.java
@@ -0,0 +1,17 @@
+package DesignPatterns.Creational.prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColorStore {
+    private static Map<String, Color> colorMap = new HashMap<>();
+    
+    static{
+        colorMap.put("Blue", new BlueColor());
+        colorMap.put("Black", new BlackColor());
+    }
+
+    public static Color getColor(String colorName){
+        return (Color)colorMap.get(colorName).clone();
+    }
+}
