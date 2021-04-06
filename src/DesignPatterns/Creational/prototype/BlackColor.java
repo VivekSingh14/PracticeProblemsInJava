2cc4b009f48db2b2742e2be08f42c349b4446614
vicky1411 <vivek.singh141@yahoo.com>
2021-04-06 23:12:24 +0530

    adding the prototype design pattern, BlueColor and BlackColor added with version 0.0.3.15

---
+++ src/DesignPatterns/Creational/prototype/BlackColor.java
@@ -0,0 +1,11 @@
+package DesignPatterns.Creational.prototype;
+
+public class BlackColor extends Color{
+    public BlackColor(){
+        this.colorName = "Black";
+    }
+
+    void addColor(){
+        System.out.println("Black color added.");
+    }
+}
---
+++ src/DesignPatterns/Creational/prototype/BlueColor.java
@@ -0,0 +1,11 @@
+package DesignPatterns.Creational.prototype;
+
+public class BlueColor extends Color{
+    public BlueColor(){
+        this.colorName = "blue";
+    }
+
+    void addColor(){
+        System.out.println("Blue color added.");
+    }
+}
