6eb71e0615c6d113acd9d2b70786c5d92c0b137b
vicky1411 <vivek.singh141@yahoo.com>
2021-04-10 11:11:23 +0530

    added the function to findout factorial using recursion.

---
+++ src/basic_programs/FactorialUsingRecur.java
@@ -0,0 +1,21 @@
+package basic_programs;
+
+import java.util.Scanner;
+
+public class FactorialUsingRecur {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num  = sc.nextInt();
+        System.out.println(factorial(num));
+        sc.close();
+    }    
+
+    public static int factorial(int num){
+        if(num == 1){
+            return 1;
+        }
+
+        return num*factorial(num-1);
+    }
+}
