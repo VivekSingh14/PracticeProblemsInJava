56fcf32c7275cdf07273a539d71221d5f80014f0
vicky1411 <vivek.singh141@yahoo.com>
2021-04-15 07:19:35 +0530

    added the pattern8 with version 0.1.0.6

---
+++ src/basic_programs/Pattern8.java
@@ -0,0 +1,30 @@
+package basic_programs;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the rows");
+        int rows  = sc.nextInt();
+        int temp = rows;
+        for(int i = 1; i <= rows; i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            
+            for(int k=temp; k>=1; k--){
+                System.out.print("*");
+            }
+            for(int l=temp-1; l >=1; l--){
+                System.out.print("*");
+            }
+            temp--;
+            System.out.println();
+
+
+        }
+
+        sc.close();
+    }
+}
