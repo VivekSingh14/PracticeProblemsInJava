214be808e236c6926ec4b47f6c66928629ad23c5
vicky1411 <vivek.singh141@yahoo.com>
2021-04-17 22:29:09 +0530

    revision with version 0.0.0.41

--- src/basic_programs/ReverseArray.java
+++ src/basic_programs/ReverseArray.java
@@ -2,10 +2,11 @@ package basic_programs;
 
 public class ReverseArray {
     public static void main(String args[]){
-        int arr[] = {1,2,3,4,5,6};
-         
+        //revised one time
+        
+        int arr[] = {1,2,3,4,5,6}; 
         int j = arr.length-1;
-        for(int i =0 ;i< arr.length/2; i++){
+        for(int i =0; i< arr.length/2; i++){
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
