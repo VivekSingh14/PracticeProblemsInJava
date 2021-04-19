220a548755e3b107f4b7f33a634127417d63e206
vicky1411 <vivek.singh141@yahoo.com>
2021-04-19 23:03:59 +0530

    complex to understand with version 0.0.0.44

--- src/basic_programs/MaxHeap.java
+++ src/basic_programs/MaxHeap.java
@@ -2,25 +2,29 @@ package basic_programs;
 
 public class MaxHeap {
     public static void main(String args[]){
-        int arr[] = {40, 35, 15,  30, 10, 12, 6, 5, 20, 50};
+        int arr[] = {40, 35, 15, 30, 10, 12, 6, 5, 20};
         System.out.println("Heap before insertion "+ arr[arr.length-1]);
         for(int i = 0; i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
-
+        System.out.println();
         System.out.println("Heap after insertion ");
         insert(arr, arr.length);
     }
 
     public static void insert(int arr[], int len){
-        int temp = arr[len-1];
         int i = len-1;
 
-        while(i > 1 && temp > arr[i/2]){
-            arr[i] = arr[i/2];
-            i = i/2;
+        while(i > 1 && arr[(i)/2] < arr[i]){
+            //arr[i] = arr[(i-1)/2];
+            int temp = arr[i];
+            arr[i] = arr[(i)/2];
+            arr[(i)/2] = temp;
+            i = (i)/2;
         }
-        arr[i-1] = temp;
+            int temp1 = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp1;
 
         for(int j=0;j<arr.length;j++){
                 System.out.print(arr[j]+" ");
