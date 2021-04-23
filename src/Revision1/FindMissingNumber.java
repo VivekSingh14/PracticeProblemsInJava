3ebaefe8dd887f7e789ea2b35aca90d117bfa554
vicky1411 <vivek.singh141@yahoo.com>
2021-04-23 22:30:45 +0530

    findMissingNumber revised with version 0.0.0.0.2

---
+++ src/Revision1/FindMissingNumber.java
@@ -0,0 +1,13 @@
+public class FindMissingNumber {
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20};
+        int actualSum = 0;
+        for(int a : arr){
+            actualSum += a;
+        }
+
+        int expectedSum = ((arr.length + 1) * (arr.length+2)) / 2;
+
+        System.out.println("Missing Number is: "+ (expectedSum - actualSum));
+    }
+}
