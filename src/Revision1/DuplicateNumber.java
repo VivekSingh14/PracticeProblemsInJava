0d82f5d6f21afba0ddb7ee9ba07192f643398884
vicky1411 <vivek.singh141@yahoo.com>
2021-04-23 23:01:28 +0530

    revised finding duplicate number with version 0.0.0.0.3

--- src/Revision1/DuplicateNumber.java
+++ src/Revision1/DuplicateNumber.java
@@ -1,6 +1,6 @@
 public class DuplicateNumber {
     public static void main(String args[]){
-        int arr[] = {1,2,3,4,5,6,7,3,9};
+        int arr[] = {1,2,3,4,5,6,7,3,9,10};
         for(int i=0; i< arr.length; i++){
             for(int j = 0; j< arr.length; j++){
                 if(arr[i] == arr[j] && i != j){
