7f5e5c157a66b16b9bbedb38c42338d2e08bac3d
vicky1411 <vivek.singh141@yahoo.com>
2021-04-24 23:55:17 +0530

    revised, to find the all pairs whose sum is equal to given number with 0.0.0.0.7

---
+++ src/Revision1/SumOfPairtogivenNumber.java
@@ -0,0 +1,14 @@
+public class SumOfPairtogivenNumber {
+    public static void main(String args[]){
+        int arr[] = {2,4,3,6,1,5,9,0};
+        int num = 9;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]== num){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+    }
+}
