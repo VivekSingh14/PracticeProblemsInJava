8ad82bc6b46639d8ab25385388c541f4e247385f
vicky1411 <vivek.singh141@yahoo.com>
2021-04-25 23:45:19 +0530

    commenting and bug fixing with version 0.0.0.46

--- src/Revision1/FindMissingNumber.java
+++ src/Revision1/FindMissingNumber.java
@@ -1,5 +1,6 @@
 public class FindMissingNumber {
     public static void main(String args[]){
+        //To find the missing number from n natural numbers
         int arr[] = {1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20};
         int actualSum = 0;
         for(int a : arr){
