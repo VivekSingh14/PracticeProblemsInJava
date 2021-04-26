f979036269d78680f9f80b95007b6170412460ea
vicky1411 <vivek.singh141@yahoo.com>
2021-04-26 21:58:36 +0530

    commenting and bug fixing with version 0.0.0.48

--- src/Revision1/SumOfPairtogivenNumber.java
+++ src/Revision1/SumOfPairtogivenNumber.java
@@ -1,5 +1,6 @@
 public class SumOfPairtogivenNumber {
     public static void main(String args[]){
+        // to find the given sum from pair of numbers
         int arr[] = {2,4,3,6,1,5,9,0};
         int num = 9;
 
