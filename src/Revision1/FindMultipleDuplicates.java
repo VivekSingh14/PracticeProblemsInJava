7e20dedc62082a78050affd1f566d02c80dc198e
vicky1411 <vivek.singh141@yahoo.com>
2021-04-29 22:07:35 +0530

    commenting and bug fixing with version 0.0.0.49

--- src/Revision1/FindMultipleDuplicates.java
+++ src/Revision1/FindMultipleDuplicates.java
@@ -1,6 +1,6 @@
 public class FindMultipleDuplicates {
     public static void main(String args[]){
-        //added the method to find out multiple duplicates
+        //added the method to find out multiple duplicates.
         int arr[] = {1,4,3,1,2,5,6,2,7,0,5};
         for(int i=0;i<arr.length;i++){
             for(int j=i+1; j<arr.length;j++){
