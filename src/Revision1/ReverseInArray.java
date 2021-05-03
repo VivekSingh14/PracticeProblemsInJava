14c7cb2fc3e78f0283d9e7a9c87409fb52343b96
vicky1411 <vivek.singh141@yahoo.com>
2021-05-03 22:36:13 +0530

    revised the reverse of array in place with version 0.0.0.0.9

---
+++ src/Revision1/ReverseInArray.java
@@ -0,0 +1,15 @@
+public class ReverseInArray {
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        int len = arr.length-1;
+        for(int i=0; i < arr.length/2; i++){
+            int temp = arr[i];
+            arr[i] = arr[len];
+            arr[len] = temp;
+            len--; 
+        }
+
+        for(int i=0;i<arr.length;i++)
+        System.out.print(arr[i]+" ");
+    }
+}
