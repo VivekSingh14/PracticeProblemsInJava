e39f951547e5b321e5088b9d137d4a186ecfc9a6
vicky1411 <vivek.singh141@yahoo.com>
2021-05-06 22:59:07 +0530

    commenting and bug fixing

--- src/basic_programs/Prime.java
+++ src/basic_programs/Prime.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Prime {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        //rvision started
         System.out.println("Enter the number");
         int num = sc.nextInt();
         boolean flag = false;
