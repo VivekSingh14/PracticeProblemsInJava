5d9444e3d06393d8b2877453d54aa0821380dbfc
vicky1411 <vivek.singh141@yahoo.com>
2021-05-08 22:04:18 +0530

    added the method to check ascii values

---
+++ src/Memory_Learn/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String args[]){
+        String str  = "vivek";
+        System.out.println((int)str.charAt(0));
+        System.out.println((int)str.charAt(2));
+    }
+}
