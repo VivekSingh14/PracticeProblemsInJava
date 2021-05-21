25bd9eafa622d7839b3a4669e18ea7f01061c762
vicky1411 <vivek.singh141@yahoo.com>
2021-05-21 07:25:55 +0530

    updated DuplicateCharacterinString

--- src/Revision1/DupicateCharacterInString.java
+++ src/Revision1/DupicateCharacterInString.java
@@ -2,6 +2,19 @@ package Revision1;
 
 public class DupicateCharacterInString {
     public static void main(String args[]){
-        
+        int arr[] = new int[26];
+        String str = "abcdab";
+
+        for(int i=0;i<str.length();i++){
+            arr[str.charAt(i)-97]+=1;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > 1){
+                int temp = 97+i;
+                System.out.print((char)temp+" ");
+            }
+        }
+        System.out.println();
     }
 }
