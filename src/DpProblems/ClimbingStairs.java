593c67940273f677890b9cb3a066e3203e51cb98
vicky1411 <vivek.singh141@yahoo.com>
2021-06-10 22:47:11 +0530

    added the method to find the ways to climb  stairs

---
+++ src/DpProblems/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package DpProblems;
+
+public class ClimbingStairs {
+    public static void main(String args[]){
+        int s=2, m=2;
+
+        System.out.println("Number of ways =  "+countWays(s,m));
+    }
+
+    public static int countWays(int s, int m){
+        return countWaysUtil(s+1, m);
+    }
+
+    public static int countWaysUtil(int n, int m){
+        int res[] = new int[n];
+        res[0] = 1;
+        res[1] = 1;
+        for(int i=2; i< n;i++){
+            res[i] = 0;
+            for(int j=1;j<=m && j <=i; j++){
+                res[i] = res[i] + res[i-j];
+            }
+        }
+        return res[n-1];
+    }
+}
