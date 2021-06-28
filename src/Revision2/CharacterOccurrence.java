327284ad4184582c5a8fe47a36502614c45580b2
vicky1411 <vivek.singh141@yahoo.com>
2021-06-28 23:58:17 +0530

    update in the character occurence method

--- src/Revision2/CharacterOccurrence.java
+++ src/Revision2/CharacterOccurrence.java
@@ -5,11 +5,15 @@ public class CharacterOccurrence {
         String str = "abaabccccffeefbbe";
         char arr[] = str.toCharArray();
 
-        int table1[] = new int[27];
+        int table1[] = new int[150];
         for(int i=0; i<arr.length;i++  ){
             table1[arr[i]]++;
         }
 
-        
+        for(int i=0;i<table1.length;i++ ){
+            System.out.println(table1[i]);
+        }
+
+
     }
 }
