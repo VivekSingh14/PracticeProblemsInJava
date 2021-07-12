a99656124eb3ba403e93f600bc0309de897e4999
vicky1411 <vivek.singh141@yahoo.com>
2021-07-12 08:38:52 +0530

    CM:  Adding Map into TestingPurpose on feature branch

--- src/Overriding/TestingPurposeOnly.java
+++ src/Overriding/TestingPurposeOnly.java
@@ -1,5 +1,6 @@
 package Overriding;
 
+import java.util.HashMap;
 //import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -8,7 +9,11 @@ public class TestingPurposeOnly {
     public static void main(String args[]){
         /*ArrayList<Integer> list1 = new ArrayList<>();*/
         Queue<Integer> que1 = new LinkedList<>();
-
+        HashMap<Integer, String> map1 = new HashMap<Integer, String>();
+        map1.put(1, "Vivek");
+        map1.put(2, "Singh");
+        map1.put(3, "GsLab");
+        map1.put(4, "Pune");
     
         que1.add(10);
         que1.add(20);
