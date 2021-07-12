fa368e19b2f9213752a50f5fa67626d9192ce3fb
vicky1411 <vivek.singh141@yahoo.com>
2021-07-12 23:15:00 +0530

    CM:  added the priority and named the thread

--- src/multithreading_Basics/Basic1.java
+++ src/multithreading_Basics/Basic1.java
@@ -7,6 +7,8 @@ public class Basic1 extends Thread{
         rollno = 1;
         System.out.println("checking inside run method whether it is alive or not "+isAlive());
         System.out.println(rollno);
+        System.out.println("Inside the run method "+getName());
+
     }
 
     public static void main(String args[]){
@@ -17,5 +19,14 @@ public class Basic1 extends Thread{
         System.out.println("Thread ID "+obj.getId());
         System.out.println("Thread Priority "+obj.getPriority());
         System.out.println("Thread is alive or not from main method "+obj.isAlive());
+
+
+        System.out.println("**************Setting up the name*************** ");
+        obj.setName("Mini Thread");
+        
+        System.out.println(Basic1.currentThread());
+        System.out.println("Inside the main method "+obj.getName());
+
+
     }
 }
