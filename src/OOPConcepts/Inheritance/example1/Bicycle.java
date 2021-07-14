05f7294edc602cdc4b973be82f7062803d4eb431
vicky1411 <vivek.singh141@yahoo.com>
2021-07-14 21:38:12 +0530

    CM: Add the Inheritance UseCase

---
+++ src/OOPConcepts/Inheritance/example1/Bicycle.java
@@ -0,0 +1,12 @@
+package OOPConcepts.Inheritance.example1;
+
+public class Bicycle {
+    public int gear;
+    public int speed;
+
+    public Bicycle(int gear, int speed){
+        this.gear = gear;
+        this.speed = speed;
+    }
+    
+}
