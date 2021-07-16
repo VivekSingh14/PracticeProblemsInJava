040392bf939d16bb250865881f5192e08350108a
vicky1411 <vivek.singh141@yahoo.com>
2021-07-16 21:56:31 +0530

    CM:  Add Polymorphism example

---
+++ src/OOPConcepts/polymorphism/example1/Father.java
@@ -0,0 +1,8 @@
+package OOPConcepts.polymorphism.example1;
+
+public class Father extends Man{
+    public void drive(){
+        System.out.println("He is driving as father taking his kids for shopping from Man class.");
+    }
+    
+}
---
+++ src/OOPConcepts/polymorphism/example1/Husband.java
@@ -0,0 +1,7 @@
+package OOPConcepts.polymorphism.example1;
+
+public class Husband extends Man{
+    public void drive(){
+        System.out.println("He is driving as Husband for attending some function with his wife from class Husband. ");
+    }
+}
