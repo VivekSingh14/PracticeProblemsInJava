eacd2cb163251965c13814d546c29f1261753332
vicky1411 <vivek.singh141@yahoo.com>
2021-07-28 08:03:13 +0530

    CM: adding print even odd using two threads

---
+++ src/OOPConcepts/multithreading/example3/One.java
@@ -0,0 +1,32 @@
+package OOPConcepts.multithreading.example3;
+
+public class One implements Runnable{
+    private int max;
+    private Printer print;
+    private boolean isEvenNumber;
+    public One(int max, Printer print, boolean isEvenNumber) {
+        this.max = max;
+        this.print = print;
+        this.isEvenNumber = isEvenNumber;
+    }
+    @Override
+    public void run() {
+        int number = isEvenNumber == true ? 2 : 1;
+        while (number <= max) {
+
+            if (isEvenNumber) {
+                //System.out.println("Even :"+ Thread.currentThread().getName());
+                print.printEven(number);
+                //number+=2;
+            } else {
+                //System.out.println("Odd :"+ Thread.currentThread().getName());
+                print.printOdd(number);
+                // number+=2;
+            }
+            number += 2;
+        }
+        
+    }
+    
+    
+}
